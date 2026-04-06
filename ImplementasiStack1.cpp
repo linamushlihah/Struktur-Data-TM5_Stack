c486b47f82f8a7496e7d7c94848ed7042bff80be
linamushlihah <linamushlihah@gmail.com>
2026-04-06 21:51:14 +0700

    Update ImplementasiStack1.cpp

--- ImplementasiStack1.cpp
+++ ImplementasiStack1.cpp
@@ -8,7 +8,6 @@ using namespace std;
 #define MAX 10
 int size = 0;
 
-// Creating a stack
 struct stack {
   int items[MAX];
   int top;
@@ -19,7 +18,6 @@ void createEmptyStack(st *s) {
   s->top = -1;
 }
 
-// Check if the stack is full
 int isfull(st *s) {
   if (s->top == MAX - 1)
     return 1;
@@ -27,7 +25,6 @@ int isfull(st *s) {
     return 0;
 }
 
-// Check if the stack is empty
 int isempty(st *s) {
   if (s->top == -1)
     return 1;
@@ -35,7 +32,6 @@ int isempty(st *s) {
     return 0;
 }
 
-// Add elements into stack
 void push(st *s, int newitem) {
   if (isfull(s)) {
     cout << "STACK FULL";
@@ -46,7 +42,6 @@ void push(st *s, int newitem) {
   size++;
 }
 
-// Remove element from stack
 void pop(st *s) {
   if (isempty(s)) {
     cout << "\n STACK EMPTY \n";
@@ -58,7 +53,6 @@ void pop(st *s) {
   cout << endl;
 }
 
-// Print elements of stack
 void printStack(st *s) {
   printf("Stack: ");
   for (int i = 0; i < size; i++) {
@@ -67,7 +61,6 @@ void printStack(st *s) {
   cout << endl;
 }
 
-// Driver code
 int main() {
   int ch;
   st *s = (st *)malloc(sizeof(st));
